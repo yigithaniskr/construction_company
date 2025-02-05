d6c63665d3a1daebec99b920ff940f4c1f77449c
yigithaniskr <yigithaniskr@gmail.com>
2025-02-05 22:16:05 +0300

    Vercel yapılandırmasını güncelle

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
   },
   server: {
     host: 'localhost',
     port: '3000'
   }
-})
+});
