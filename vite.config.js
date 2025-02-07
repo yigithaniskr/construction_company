c944da822007339ba40de19d98dbd7a5ba836286
yigithaniskr <yigithaniskr@gmail.com>
2025-02-07 22:42:25 +0300

    Vercel yapılandırmasını güncelle

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ana dizinden başlatmayı sağlar
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
+    outDir: 'dist', // Çıktı dosyalarının konumu
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
   },
-  base: '/' // Kök dizin kullanımı için ayarlayın
 });
