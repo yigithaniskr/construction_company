5d473e26a72064cae75d9e3a230a391ff5f5946c
yigithaniskr <yigithaniskr@gmail.com>
2025-02-07 23:10:34 +0300

    Vercel yapılandırmasını güncelle

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ana dizinden başlatmayı sağlar
-  build: {
-    outDir: 'dist', // Çıktı dosyalarının konumu
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
+  server: {
+    port: 3000,
+    hmr: {
+      host: "localhost",
     },
+  },
+  plugins: [],
+  base: "./", // Burayı güncelle
+  build: {
+    outDir: "dist",
   },
 });
